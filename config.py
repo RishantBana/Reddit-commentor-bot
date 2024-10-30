fb461aa96006202a730f3d948db1a482d837da4f
Rishant Bana <vrbros2@gmail.com>
2024-10-30 23:18:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "Mountain_Category889"
-password = "Vrbros29b@"
-client_id = "dqw2z-5sqPNrPNVKwEyXRQ"
-client_secret = "o1o-S12eDZpgR8xVrD2D25yBUtoDtw"
+username = "Ypur Username"
+password = "Ypur Password"
+client_id = "Your client ID"
+client_secret = "Your client secret"
